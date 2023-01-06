28dfc7317dd2408a77dc924cb3ce213555783ddf
Matees <mateespublicprofile@gmail.com>
2023-01-05 23:25:49 -0500

    added ansi colors dep from clippy

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,3 @@
 export * as fs from "https://deno.land/std@0.171.0/fs/mod.ts";
 export { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+export { colors } from "https://deno.land/x/cliffy@v0.25.7/ansi/mod.ts";
