27169a128e9bbd02e7acbc7cb85e594c0abf18d3
Matees <88842870+maytees@users.noreply.github.com>
2023-01-06 13:15:25 -0500

    added keyword token type

--- src/lang/token.ts
+++ src/lang/token.ts
@@ -1,24 +1,25 @@
 export enum TokenType {
-    TTNone,
-    TTString,
-    TTNumber,
-    TTIden,
-    TTLparen,
-    TTRparen,
-    TTEq,
-    TTAdd,
-    TTSub,
-    TTDiv,
-    TTMul,
-    TTSemi,
+  TTNone,
+  TTString,
+  TTNumber,
+  TTIden,
+  TTKeyword,
+  TTLparen,
+  TTRparen,
+  TTEq,
+  TTAdd,
+  TTSub,
+  TTDiv,
+  TTMul,
+  TTSemi,
 }
 
 export class Token {
-    public type: TokenType = TokenType.TTNone;
-    public value: string = "";
+  public type: TokenType = TokenType.TTNone;
+  public value = "";
 
-    public constructor (type: TokenType, value: string) {
-        this.type = type;
-        this.value = value;
-    }
+  public constructor(type: TokenType, value: string) {
+    this.type = type;
+    this.value = value;
+  }
 }
